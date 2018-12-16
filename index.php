1203c6a92295421fb6461852fe5832e5109a20c0
romualdhournon66 <k2001@cegetel.net>
2018-12-16 22:08:39 +0100

    mon index

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <title>PHP Partie 5 exercice 1</title>
+</head>
+<body>
+  <div align="center">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-12">
+          <h1 align="center">PHP Partie 5 exercice 1</h1>
+        </div>
+      </div>
+      <div align="center">
+        <p class="text">Créer un tableau months et l'initialiser avec les valeurs suivantes :</p>
+        <li>Janvier</li>
+        <li>Février</li>
+        <li>Mars</li>
+        <li>Avril</li>
+        <li>Mai</li>
+        <li>Juin</li>
+        <li>Juillet</li>
+        <li>Aout</li>
+        <li>Septembre</li>
+        <li>Octobre</li>
+        <li>Novembre</li>
+        <li>Décembre</li>
+
+      </div>
+      <?php
+      $months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre');
+      ?>
+    </div>
+  </div>
+</div>
+</body>
+</html>
